643cb4fd47355c3b8064671012ba972b987e1ee2
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2023-11-30 10:46:51 -0300

    exibindo a api

--- Exercicio_2/backend/parking/urls.py
+++ Exercicio_2/backend/parking/urls.py
@@ -4,6 +4,7 @@ from .views  import *
 app_name="parking"
 
 urlpatterns = [
+    
     path('api/v1/customer/', CustomerView.as_view()),
     path('api/v1/plan/', PlanView.as_view()),
     path('api/v1/vehicle/', VehicleView.as_view()),
---
+++ Exercicio_2/frontend/src/views/ParkinDetails.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        {{ $route.params.name }}
+        {{ $route.params.card_id }}
+    </div>
+</template>
+
+<script>
+</script>
